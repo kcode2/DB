219b9c9fae8dd4ba2c0f880fc09742226d0abbfe
kcode2 <134388234+kcode2@users.noreply.github.com>
2023-06-01 15:56:49 +0530

    Update TCO.py

--- TCO.py
+++ TCO.py
@@ -3,18 +3,21 @@ import streamlit as st
 import sqlite3
 def main():
    
-    conn = sqlite3.connect('college.db', check_same_thread=False)
+    conn = sqlite3.connect('college1.db', check_same_thread=False)
+    
     c = conn.cursor()
 
-    c.execute('CREATE TABLE IF NOT EXISTS college (COLLEGECODE Text,COLLEGENAME Text,BRANCHCODE Text,OC number,BC number,BCM number,MBCV number,MBCDNC number,MBC number,SC number,SCA number,ST number)')
+    c.execute('CREATE TABLE IF NOT EXISTS college (Year number,COLLEGECODE Text,COLLEGENAME Text,BRANCHCODE Text,OC number,BC number,BCM number,MBCV number,MBCDNC number,MBC number,SC number,SCA number,ST number)')
     #"COLLEGECODE"	COLLEGE NAME	BRANCH CODE	OC	BC	BCM	MBC	SC	SCA	ST
     conn.commit()
     f=st.file_uploader(":green[Upload a Invoice file]")
     if f is not None:
+        #df=pd.read_excel(f)
         df=pd.read_csv(f,encoding='cp1252')
     st.write(df)
-    df.to_sql('college',conn,if_exists='append',index=False)
+    df.to_sql('college1',conn,if_exists='append',index=False)
     
 
 if __name__ == '__main__':
 	main()
+
